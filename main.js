63c1942daa1c0f175b023b5b16f3e837833ddc03
gabrielhicks <gabrielhicks@mail.com>
2020-10-08 21:37:26 -0400

    hearts

--- main.js
+++ main.js
@@ -3,7 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal")
+function setHidden() {
+  modal.classList.add("hidden");
+}
 
+function unsetHidden() {
+  modal.classList.remove("hidden");
+}
+
+function redHeart(e) {
+  e.target.classList.add("activated-heart")
+  e.target.innerText = FULL_HEART;
+}
+
+function greyHeart(e) {
+  e.target.classList.remove("activated-heart");
+  e.target.innerText = EMPTY_HEART;
+}
+setHidden();
+
+
+document.addEventListener("click", e => {
+  if(e.target.matches(".like-glyph")){
+  mimicServerCall()
+    .then(message => {
+      console.log(message)
+      if(e.target.matches(".activated-heart")) {
+        greyHeart(e);
+      } else {
+        redHeart(e);
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      modal.classList.remove("hidden");
+      modal.innerText = error;
+      setTimeout(setHidden, 5000)
+    })
+  }
+})
 
 
 
